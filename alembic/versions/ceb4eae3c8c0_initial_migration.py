c7153767b8e5848ad766d918f47b29a24f67c498
AntiViruS90 <131804906+AntiViruS90@users.noreply.github.com>
2024-11-11 23:48:34 +0300

    refactoring alembic

---
+++ alembic/versions/ceb4eae3c8c0_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial migration
+
+Revision ID: ceb4eae3c8c0
+Revises: cf64559c5eec
+Create Date: 2024-11-11 23:35:13.009554
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ceb4eae3c8c0'
+down_revision = 'cf64559c5eec'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
